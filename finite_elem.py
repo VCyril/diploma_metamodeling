eff93b78f8f20e86f52d89ed97140796473295c2
VCyril <ver17hovsk@gmail.com>
2021-11-01 07:28:42 +0300

    Refactoring geometry creating 1 (segfault)

--- finite_elem.py
+++ finite_elem.py
@@ -8,36 +8,59 @@ model_name = 'model'
 
 
 def create_sensor_geometry():
-    model = pyc.FeaModel(model_name, ccx, cgx, gmsh)
-    model.set_units('m')
+    # We'll be modeling a rotating jet engine part
+    model = pyc.FeaModel(model_name)
+    model.set_units('mm')
+    show_gui = True
 
-    load_lines = []
-    constr_lines = []
+    # радиальные размеры
+    ir = 35  # внутренний размер мембраны
+    rf = 15
+    or_ = 50  # внешний радиус датчика в целом
+    r_groove = 20  # радиус центра внешней проточки
+    r_neck = 8.3  # радиус шеи датчика
+    r_head = 12.5  # радиус головы датчика
+    t_shoulder = 14.36
+
+    # осевые размеры
+    h_inner = 27  # высота внутренней полости
+    h_shoulder = 44  # высота плеч датчика
+    h_groove = 29.4  # высота  угла внешней проточки
+    h_neck = 41.1  # высота шеи
+    h = 50  # высота датчика в целом
+
+    m50_loop = [
+        [ir, 0],
+        [0, -h_inner],
+        [rf, 0],
+        [0, h_shoulder],
+        [-t_shoulder, 0],
+        [-(or_ - t_shoulder - r_groove), h_groove - h_shoulder],
+        [(r_neck - r_groove), (h_neck - h_groove)],
+        [(r_head - r_neck), 5],
+        [0, 5],
+        [-r_head, 0],
+        [0, -(h_neck + 10 - h_inner)]
+    ]
+
     part = pyc.Part(model)
-    part.goto(0.04, 0)
+    part.goto(0, h_inner)
+    lines = []
+    for [drad, dax] in m50_loop:
+        [L, p1, p2] = part.draw_line_delta(drad, dax)
+        lines.append(L)
 
-    [L1, _, _] = part.draw_line_rad(0.01)
-    constr_lines.append(L1)
-    part.draw_line_ax(0.04)
-    part.draw_line_rad(-0.04)
-    # part.draw_line_to(20, 40)
-    # part.draw_line_to(10, 50)
+    # fillet_list = [[0, 1, 3], [5, 6, 3], [6, 7, 3]]
+    # for [i1, i2, rad] in fillet_list:
+    #     part.fillet_lines(lines[i1], lines[i2], rad)
 
-    part.draw_line_ax(0.01)
-    [L1, _, _] = part.draw_line_rad(-0.01)
-    load_lines.append(L1)
-    [L1, _, _] = part.draw_line_ax(-0.02)
-    constr_lines.append(L1)
-    part.draw_line_rad(0.04)
-    part.draw_line_ax(-0.03)
-
-    return model, part, load_lines, constr_lines
+    return model, part
 
 
-def set_loads_and_constraints(model, load_lines, constr_lines):
-    model.set_load('press', load_lines, 63661977)
-    model.set_constr('fix', constr_lines[0], 'y')
-    model.set_constr('fix', constr_lines[1], 'x')
+def set_loads_and_constraints(model, part):
+    model.set_load('press', part.right, 63661977)
+    model.set_constr('fix', part.bottom, 'x')
+    model.set_constr('fix', part.left, 'y')
 
 
 def plot_model_geometry(model, show_gui):
@@ -58,11 +81,10 @@ def set_mat_props(model, part):
 def set_elem_props(model, part, elem_shape, elem_size):
     model.set_eshape(elem_shape, 2)
     model.set_etype('axisym', 'A0')
-    model.set_etype('plstrain', part, 0.1)
     try:
-        model.mesh(elem_size, "esize", 'gmsh')
-    except Exception:
-        return 'Shit happend'
+        model.mesh(elem_size, 'fineness', 'gmsh')
+    except Exception as excp:
+        return str(excp)
     return None
 
 
