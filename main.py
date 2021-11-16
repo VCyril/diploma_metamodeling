14e24d01f4f9b98402c7bb26ef0aea5959bc71ae
VCyril <ver17hovsk@gmail.com>
2021-11-16 15:15:18 +0300

    creating samples improved

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import plotting as myplt
 import matplotlib.pyplot as plt
 import finite_elem as fin
 import numpy as np
+import pyDOE as doe
 import converting_results as conv
 import sys
 
@@ -20,23 +21,57 @@ H_GROOVE = 30
 H_NECK = 40
 H = 50
 
-ir = np.arange(IR * 0.95, IR * 1.05, IR * 0.1 / SAMPLES_NUM)
-or_ = np.arange(OR_ * 0.95, OR_ * 1.05, OR_ * 0.1 / SAMPLES_NUM)
-r_groove = np.arange(R_GROOVE * 0.95, R_GROOVE * 1.05, R_GROOVE * 0.1 / SAMPLES_NUM)
-r_neck = np.arange(R_NECK * 0.95, R_NECK * 1.05, R_NECK * 0.1 / SAMPLES_NUM)
-t_shoulder = np.arange(T_SHOULDER * 0.95, T_SHOULDER * 1.05, T_SHOULDER * 0.1 / SAMPLES_NUM)
-h_inner = np.arange(H_INNER * 0.95, H_INNER * 1.05, H_INNER * 0.1 / SAMPLES_NUM)
-h_shoulder = np.arange(H_SHOULDER * 0.95, H_SHOULDER * 1.05, H_SHOULDER * 0.1 / SAMPLES_NUM)
-h_groove = np.arange(H_GROOVE * 0.95, H_GROOVE * 1.05, H_GROOVE * 0.1 / SAMPLES_NUM)
-h_neck = np.arange(H_NECK * 0.95, H_NECK * 1.05, H_NECK * 0.1 / SAMPLES_NUM)
-h = np.arange(H * 0.95, H * 1.05, H * 0.1 / SAMPLES_NUM)
+VAR_RANGE_COEF = 0.05
+VAR_UPPER = 1 + VAR_RANGE_COEF
+VAR_LOWER = 1 - VAR_RANGE_COEF
+
+ir = np.arange(IR * VAR_LOWER, IR * VAR_UPPER, IR * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+or_ = np.arange(OR_ * VAR_LOWER, OR_ * VAR_UPPER, OR_ * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+r_groove = np.arange(R_GROOVE * VAR_LOWER, R_GROOVE * VAR_UPPER, R_GROOVE * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+r_neck = np.arange(R_NECK * VAR_LOWER, R_NECK * VAR_UPPER, R_NECK * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+t_shoulder = np.arange(T_SHOULDER * VAR_LOWER, T_SHOULDER * VAR_UPPER, T_SHOULDER * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+h_inner = np.arange(H_INNER * VAR_LOWER, H_INNER * VAR_UPPER, H_INNER * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+h_shoulder = np.arange(H_SHOULDER * VAR_LOWER, H_SHOULDER * VAR_UPPER, H_SHOULDER * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+h_groove = np.arange(H_GROOVE * VAR_LOWER, H_GROOVE * VAR_UPPER, H_GROOVE * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+h_neck = np.arange(H_NECK * VAR_LOWER, H_NECK * VAR_UPPER, H_NECK * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+h = np.arange(H * VAR_LOWER, H * VAR_UPPER, H * 2 * VAR_RANGE_COEF / SAMPLES_NUM)
+
+
+def create_samples():
+    var_lower = np.array([IR * VAR_LOWER,
+                          OR_ * VAR_LOWER,
+                          R_GROOVE * VAR_LOWER,
+                          R_NECK * VAR_LOWER,
+                          T_SHOULDER * VAR_LOWER,
+                          H_INNER * VAR_LOWER,
+                          H_SHOULDER * VAR_LOWER,
+                          H_GROOVE * VAR_LOWER,
+                          H_NECK * VAR_LOWER,
+                          H * VAR_LOWER])
+    var_upper = np.array([IR * VAR_UPPER,
+                          OR_ * VAR_UPPER,
+                          R_GROOVE * VAR_UPPER,
+                          R_NECK * VAR_UPPER,
+                          T_SHOULDER * VAR_UPPER,
+                          H_INNER * VAR_UPPER,
+                          H_SHOULDER * VAR_UPPER,
+                          H_GROOVE * VAR_UPPER,
+                          H_NECK * VAR_UPPER,
+                          H * VAR_UPPER])
+
+    n = len(var_upper)
+    lhd = doe.lhs(n, samples=SAMPLES_NUM)
+    lhd = lhd * (var_upper - var_lower) + var_lower
+    # samples = [[var_lower[ind] + (var_upper[ind] - var_lower[ind]) * lhd_point[ind]
+    #             for ind in range(n)] for lhd_point in lhd]
+    return lhd
 
 
 def run_simulation(_ir, _or, _r_groove, _r_neck, _t_shoulder, _h_inner, _h_shoulder, _h_groove, _h_neck, _h,
                    load):
     model, part = fin.create_sensor_geometry(_ir, _or, _r_groove, _r_neck, _t_shoulder,
                                              _h_inner, _h_shoulder, _h_groove, _h_neck, _h)
-    # fin.plot_model_geometry(model, False)
+    fin.plot_model_geometry(model, False)
     fin.set_loads_and_constraints(model, part, load / (3.1415 * _r_neck ** 2))
     fin.set_mat_props(model, part)
 
@@ -58,6 +93,11 @@ def run_simulation(_ir, _or, _r_groove, _r_neck, _t_shoulder, _h_inner, _h_shoul
 
 rated_load = [10000, 20000, 30000, 40000, 50000]
 
-for i in range(10):
-    run_simulation(ir[i], or_[i], r_groove[i], r_neck[i], t_shoulder[i],
-                   h_inner[i], h_shoulder[i], h_groove[i], h_neck[i], h[i], rated_load[0])
+samples = create_samples()
+
+# print(samples[0])
+
+for i in range(len(samples)):
+    run_simulation(samples[i][0], samples[i][1], samples[i][2], samples[i][3],
+                   samples[i][4], samples[i][5], samples[i][6], samples[i][7],
+                   samples[i][8], samples[i][9], rated_load[0])
